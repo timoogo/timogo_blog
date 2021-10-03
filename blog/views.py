37acc08f081e896bc8f8f09a6e8a68b74201124f
Timothée Gaultier <tiimogo@gmail.com>
2021-10-03 19:45:05 +0200

    css + post start

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
-def index(request):
-    if request.method == "GET":
-        return render(request, 'blog/index.html')
+from .models import Post
+
+def post_list_index(request):
+    post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    return render(request, 'blog/post_list.html', {'posts' : post_list})
--- timogo/urls.py
+++ timogo/urls.py
@@ -21,6 +21,6 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name ='index')
+    path('', views.post_list_index, name ='post_list_index')
 ]
 urlpatterns += staticfiles_urlpatterns()
